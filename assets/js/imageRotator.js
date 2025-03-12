fcb677c6fd546f364764df4e9620f14a5ba33bc4
PebblesDV <pebblesdevries@gmail.com>
2025-03-12 09:13:46 +0100

    push

--- assets/js/imageRotator.js
+++ assets/js/imageRotator.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-let foodImages = [
-    "assets/img/food.png", 
+document.addEventListener("DOMContentLoaded", function () {
+  let foodImages = [
+    "assets/img/food.png",
     "assets/img/Breakfast2.webp", //change to the name of the 2nd image
-    "assets/img/Breakfast3.webp" //you can go on and on w more images or whatever
-];
+    "assets/img/Breakfast3.webp", //you can go on and on w more images or whatever
+  ];
 
-let currentImageIndex = 0;
+  let currentImageIndex = 0;
 
-function changeFoodImage() {
+  function changeFoodImage() {
     currentImageIndex = (currentImageIndex + 1) % foodImages.length;
-    document.querySelector('.food').src = foodImages[currentImageIndex];
-}
+    document.querySelector(".food").src = foodImages[currentImageIndex];
+  }
 
-//After how much time it changes. This is 1 second (im pretty sure its in milisecond idk god help me)
-setInterval(changeFoodImage, 100);
+  //After how much time it changes. This is 1 second (im pretty sure its in milisecond idk god help me)
+  setInterval(changeFoodImage, 3000);
 });
